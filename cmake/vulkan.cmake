1821692f1977acb9100b5ce61880b5b761bdc927
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-07-17 13:27:13 +0200

    rely more on system libs for deps

--- cmake/vulkan.cmake
+++ cmake/vulkan.cmake
@@ -1,7 +1,7 @@
 set(VULKAN_HEADERS_ENABLE_MODULE      ON)
 set(VULKAN_HEADERS_ENABLE_MODULE_STD OFF)
 
-# find_package(Vulkan 1.4.322 QUIET)
+find_package(Vulkan 1.4.322 QUIET)
 if (NOT Vulkan_FOUND)
     include(FetchContent)
     FetchContent_Declare(vulkan-headers
@@ -12,6 +12,15 @@ if (NOT Vulkan_FOUND)
         EXCLUDE_FROM_ALL
         SYSTEM)
     FetchContent_MakeAvailable(vulkan-headers)
+elseif(NOT TARGET Vulkan::HppModule)
+    add_library(Vulkan-HppModule)
+    add_library(Vulkan::HppModule ALIAS Vulkan-HppModule)
+    target_sources(Vulkan-HppModule
+        PUBLIC
+        FILE_SET cxx_module_vulkan
+        TYPE CXX_MODULES
+        BASE_DIRS ${Vulkan_INCLUDE_DIR}
+        FILES "${Vulkan_INCLUDE_DIR}/vulkan/vulkan.cppm")
 endif()
 
 target_link_libraries(${PROJECT_NAME} PRIVATE Vulkan::HppModule)
